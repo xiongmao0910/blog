d0e0911b09430250f8e213f5f7956f11cc7413cc
xiongmao <trungwebdev@gmail.com>
2023-04-03 03:23:19 +0700

    Fixed getPosts method in post controller

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -7,8 +7,8 @@ const postController = require("../controllers/post");
 const router = express.Router();
 
 router.get("/", postController.index);
+router.get("/dashboard/:username", postController.getPosts);
 router.get("/:username/:slug", postController.getPost);
-router.get("/:username/dashboard", postController.getPosts);
 router.post("/create", postController.create);
 router.put("/edit", postController.edit);
 router.delete("/delete", postController.delete);
